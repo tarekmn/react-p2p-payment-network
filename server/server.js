e5098aea98f1074532dfcb88adfe044fcb82b85c
tarekmn <nytarek@gmail.com>
2022-11-30 13:38:20 -0600

    disable production

--- server/server.js
+++ server/server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use(routes);
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
 
 db.once('open', () => {
