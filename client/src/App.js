6b29bb5226f135e78639aee8dac52314ea773058
tarekmn <nytarek@gmail.com>
2022-11-30 14:42:26 -0600

    trial and error6

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AppProvider } from "./utils/AppContext"
-import Navigation from './components/Navigation.jsx';
+import Navigation from './components/Navigation';
 import Login from './components/Login';
 import Home from './components/Home';
 import Profile from './components/Profile';
-import Signup from './components/Signup.jsx';
+import Signup from './components/Signup';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import AllFeed from './components/AllFeed';
@@ -18,10 +18,6 @@ import './App.css'
 function App() {
 
   const [trans, setTrans] = useState()
-
-
-
-
 
   return (
     <div>
