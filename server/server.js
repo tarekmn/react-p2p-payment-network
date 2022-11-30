877fd527628315d7a721151fc49bc3ae395968f8
tarekmn <nytarek@gmail.com>
2022-11-30 13:07:14 -0600

    trial on server

--- server/server.js
+++ server/server.js
@@ -10,13 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+app.use(routes);
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
