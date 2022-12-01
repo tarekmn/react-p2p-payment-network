da4b4c10c923b268b2b3ded5f9da8cf9c18a828f
tarekmn <nytarek@gmail.com>
2022-12-01 15:55:15 -0600

    working dev

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,20 +1,13 @@
 import InfiniteScroll from "react-infinite-scroll-component";
-
-
 const Feed = ({ currentUser, setCurrentUser }) => {
   console.log(currentUser.transactions);
   console.log(currentUser);
-
   return (
     <>
-      {" "}
       <div
         id="scrollableDiv"
-
         style={{ height: 250, width: 300, overflow: "auto" }}
-
       >
-        {" "}
         <InfiniteScroll
           dataLength={currentUser.transactions.length}
           next={currentUser.transactions}
@@ -26,36 +19,29 @@ const Feed = ({ currentUser, setCurrentUser }) => {
             backgroundColor: "black",
           }}
         >
-
           {currentUser.transactions &&
             currentUser.transactions.map((t, i) => {
-
-              
               const tstyle =
                 t.creditUser._id === currentUser.id
                   ? { backgroundColor: "#00E661" }
-                  : { backgroundColor: "#CA2B29" }
-
-
+                  : { backgroundColor: "#CA2B29" };
               return (
                 <div
                   key={i}
                   className="border border-dark rounded mb-2 text-dark p-1"
                   style={tstyle}
                 >
-
                   <img
                     className="postimg"
                     src={`/stock/${currentUser.image}.png`}
                     width="35"
                     height="35"
-                    alt='stock profile'
+                    alt="stock profile"
                     style={{
                       borderRadius: "50%",
                       margin: 4,
                     }}
                   />
-
                   {t.creditUser._id === currentUser.id ? (
                     <>
                       {t.debitUser.username} sent you ${t.amount} for{" "}
@@ -64,29 +50,6 @@ const Feed = ({ currentUser, setCurrentUser }) => {
                   ) : (
                     <>
                       You sent {t.creditUser.username} ${t.amount} for{" "}
-                      {t.transactionText}
-                    </>
-                  )}
-                </div>
-
-              )
-            } 
-            )}
-
-
-               
-                  
-                  {/* is this transaction a credit ? */}
-                  {t.creditUser._id === currentUser.id ? (
-                    <>
-                      
-                      {t.debitUser.username} sent you ${t.amount} for {" "}
-                      {t.transactionText}
-                    </>
-                  ) : (
-                    <>
-                      
-                      You sent {t.creditUser.username} ${t.amount} for {" "}
                       {t.transactionText}
                     </>
                   )}
